38c3fed1473b26a0fca6df3649b1c23b164b6c98
furmmon <raphael.gavache@student.ecp.fr>
2016-09-07 16:17:22 +0200

    Swipe fonctionnel

--- demo/src/main/java/com/daimajia/swipedemo/adapter/RecyclerViewAdapter.java
+++ demo/src/main/java/com/daimajia/swipedemo/adapter/RecyclerViewAdapter.java
@@ -24,12 +24,13 @@ import com.daimajia.swipe.SimpleSwipeListener;
 import com.daimajia.swipe.SwipeLayout;
 import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
 import com.daimajia.swipedemo.R;
+import com.daimajia.swipe.implments.SwipeItemMangerImpl;
 
 import java.util.ArrayList;
 
 public class RecyclerViewAdapter extends RecyclerSwipeAdapter<RecyclerViewAdapter.SimpleViewHolder> {
 
-    public static class SimpleViewHolder extends RecyclerView.ViewHolder {
+    public class SimpleViewHolder extends RecyclerView.ViewHolder {
         SwipeLayout swipeLayout;
         LinearLayout upperLayout;
         TextView textViewPos;
@@ -61,7 +62,7 @@ public class RecyclerViewAdapter extends RecyclerSwipeAdapter<RecyclerViewAdapte
     private Context mContext;
     private ArrayList<String> mDataset;
 
-    //protected SwipeItemRecyclerMangerImpl mItemManger = new SwipeItemRecyclerMangerImpl(this);
+    //protected SwipeItemMangerImpl mItemManger = new SwipeItemMangerImpl(this);
 
     public RecyclerViewAdapter(Context context, ArrayList<String> objects) {
         this.mContext = context;
@@ -87,23 +88,23 @@ public class RecyclerViewAdapter extends RecyclerSwipeAdapter<RecyclerViewAdapte
         viewHolder.swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
             @Override
             public void onDoubleClick(SwipeLayout layout, boolean surface) {
-                Toast.makeText(mContext, "DoubleClick"+position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "DoubleClick" + position, Toast.LENGTH_SHORT).show();
             }
         });
         viewHolder.imagesms.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(mContext, "Message sent "+position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "Message sent " + position, Toast.LENGTH_SHORT).show();
                 Uri phoneNumber = Uri.parse("sms:0606060606");
                 Intent smsIntent = new Intent(Intent.ACTION_SEND, phoneNumber);
                 view.getContext().startActivity(smsIntent);
             }
         });
-        viewHolder.imagephone.setOnClickListener(new View.OnClickListener(){
+        viewHolder.imagephone.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
 
-                Toast.makeText(mContext, "Phone call "+position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "Phone call " + position, Toast.LENGTH_SHORT).show();
                 Uri phoneNumber = Uri.parse("tel:0606060606");
                 Intent callIntent = new Intent(Intent.ACTION_DIAL, phoneNumber);
                 view.getContext().startActivity(callIntent);
@@ -123,32 +124,23 @@ public class RecyclerViewAdapter extends RecyclerSwipeAdapter<RecyclerViewAdapte
         viewHolder.buttonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Toast.makeText(view.getContext(), "Want to delete? " + viewHolder.textViewData.getText().toString() + "!", Toast.LENGTH_SHORT).show();
-
-                /*System.out.println("mDataset :");
-                System.out.println(mDataset.toString());
-                System.out.println("position : ");
-                System.out.println(position);
                 mItemManger.removeShownLayouts(viewHolder.swipeLayout);
+
                 mDataset.remove(position);
-                System.out.println("notifyItemRemovedPosition");
-                notifyItemRemoved(position);
-                //notifyItemRangeChanged(position, mDataset.size());
-                System.out.println("mDataset2 :");
-                System.out.println(mDataset.toString());
-                mItemManger.closeAllItems();
-                Toast.makeText(view.getContext(), "Deleted " + viewHolder.textViewData.getText().toString() + "!", Toast.LENGTH_SHORT).show();
-                System.out.println("After notifyItemRemovedPosition");*/
-
-
-                /*mItemManger.removeShownLayouts(viewHolder.swipeLayout);
-                mDataset.remove(position);
-                notifyItemRemoved(position);
-
                 notifyItemRangeChanged(position, mDataset.size());
-
+                notifyDatasetChanged();
                 mItemManger.closeAllItems();
-                Toast.makeText(view.getContext(), "Deleted " + viewHolder.textViewData.getText().toString() + "!", Toast.LENGTH_SHORT).show();*/
+
+
+                //mItemManger.removeShownLayouts(viewHolder.swipeLayout);
+                //notifyItemRemoved(position);
+
+                //mDataset.remove(position);
+
+                //notifyItemRangeChanged(position, mDataset.size());
+
+                //mItemManger.closeAllItems();
+                //Toast.makeText(view.getContext(), "Deleted " + viewHolder.textViewData.getText().toString() + "!", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -167,3 +159,5 @@ public class RecyclerViewAdapter extends RecyclerSwipeAdapter<RecyclerViewAdapte
         return R.id.swipe;
     }
 }
+
+
